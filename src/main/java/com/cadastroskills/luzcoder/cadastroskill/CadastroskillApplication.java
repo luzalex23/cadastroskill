5bde400879c683ac4eaeeee22cb5f8c84116f94c
luzalex23 <alexluzendreyravi@gmail.com>
2023-12-15 22:45:27 -0300

    first commit

---
+++ src/main/java/com/cadastroskills/luzcoder/cadastroskill/CadastroskillApplication.java
@@ -0,0 +1,13 @@
+package com.cadastroskills.luzcoder.cadastroskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastroskills/luzcoder/cadastroskill/CadastroskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastroskills.luzcoder.cadastroskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
